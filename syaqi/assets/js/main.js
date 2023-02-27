8f93cc6a4247af903b44319292471cc95321c213
syaqi <nrsyqirh@gmail.com>
2023-02-27 14:23:46 +0800

    weheee

--- syaqi/assets/js/main.js
+++ syaqi/assets/js/main.js
@@ -245,6 +245,37 @@
     }
   }
 
+  $( '#sendMailForm' ).submit(function ( e ) {
+    var data = {
+        'name': $('#name').val(),
+        'email': $('#email').val(),
+        'subject': $('#sub').val(),
+        'message' : $('#message').val()
+    };
+    // POST data to the php file
+    $.ajax({ 
+        url: 'sendmail.php', 
+        data: data,
+        type: 'POST',
+        success: function (data) {
+			// For Notification
+            document.getElementById("sendMailForm").reset();
+            var $alertDiv = $(".mailResponse");
+            $alertDiv.show();
+            $alertDiv.find('.alert').removeClass('alert-danger alert-success');
+            $alertDiv.find('.mailResponseText').text("");
+            if(data.error){
+                $alertDiv.find('.alert').addClass('alert-danger');
+                $alertDiv.find('.mailResponseText').text(data.message);
+            }else{
+                $alertDiv.find('.alert').addClass('alert-success');
+                $alertDiv.find('.mailResponseText').text(data.message);
+            }
+        }
+    });
+    e.preventDefault();
+});
+
 
   /*--------------------------------------------------------------
     8. Light Gallery
